9b3449c9bbe53d0406a03ecd1e9b3e116524b93c
johnny-188 <80451860+johnny-188@users.noreply.github.com>
2021-11-13 14:35:47 +0700

    Add files via upload

---
+++ nhanvien.h
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Nhansu {
+public:
+	virtual void nhap() = 0;
+	virtual void xuat() = 0;
+};
+
+class ThongTin :public Nhansu {
+private:
+	string hoten;
+	string ID;
+	string gioitinh;
+	int ngay, thang, nam;
+
+public:
+	ThongTin();
+	~ThongTin();
+
+	void nhap();
+	void xuat();
+
+	string getID();
+
+
+};
+
+
+class LuongPartTime :public Nhansu {
+private:
+	int sogio;
+	int thuong;
+public:
+	LuongPartTime();
+	~LuongPartTime();
+
+	void nhap();
+	void xuat();
+	int luong();
+};
+
+
+class LuongFullTime :public Nhansu {
+private:
+	int tienthuong;
+	int songaynghi;
+public:
+	LuongFullTime();
+	~LuongFullTime();
+
+	void nhap();
+	void xuat();
+	int tluong();
+
+};
+class Node {
+private:
+	ThongTin data;
+	Node* next;
+public:
+	friend class List;
+};
+
+
+class List {
+private:
+	Node* head;
+	Node* tail;
+	long size;
+public:
+	List();
+	~List();
+
+	Node* CreateNode(ThongTin ns);
+	void SearchID();
+	bool Tim(string id);
+	void AddL();
+	void xuatNS();
+	void Sua();
+};
